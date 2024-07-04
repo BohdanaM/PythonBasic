22c211b7fbb7cb5b1d72059e27144e95edda9650
Bogdana Matveeva <b.matveeva@epicentrm.com.ua>
2024-07-04 15:19:02 -0700

    Resolving L3 HW1

---
+++ L3/HW_1.py
@@ -0,0 +1,22 @@
+first_operand = float(input("Enter first digit: "))
+operator = input("Enter operator (+, -, *, /): ")
+second_operand = float(input("Enter second digit: "))
+
+if operator == '+':
+    result = first_operand + second_operand
+elif operator == '-':
+    result = first_operand - second_operand
+elif operator == '*':
+    result = first_operand * second_operand
+elif operator == '/':
+    if second_operand == 0:
+        result = None
+        print("Can not be divided by 0")
+    else:
+        result = first_operand / second_operand
+else:
+    result = None
+    print("Entered no valid data")
+
+print(result)
+
