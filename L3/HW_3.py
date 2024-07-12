ac347181f7c6ab68a8810db36c361372c3f33289
Bogdana Matveeva <b.matveeva@epicentrm.com.ua>
2024-07-12 10:47:05 -0700

    Add more solution to L3 HW3

--- L3/HW_3.py
+++ L3/HW_3.py
@@ -12,3 +12,13 @@ else:
     second_list = lst[middle_index:]
     new_list = [first_list, second_list]
     print(new_list)
+
+
+# my_list = [1, 2, 3]
+#
+# half = len(my_list) // 2
+# if len(my_list) % 2 == 0:
+#     result = my_list[:half], my_list[half:]
+# else:
+#     result = ((my_list[:half + 1]), (my_list[half:]) * 0 + my_list[half + 1:])
+# print(result)
