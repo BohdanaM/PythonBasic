5ae6779b8fe909e5cdfda8db31596dfe07b0070d
Bogdana Matveeva <b.matveeva@epicentrm.com.ua>
2024-07-17 07:38:34 -0700

    Fix L6 HW1

--- L6/HW_1.py
+++ L6/HW_1.py
@@ -3,15 +3,14 @@ import string
 letters = input("Enter letters:")
 
 # Find the first and the last letters
-letters_pair = letters.split("-")
-first_letter = letters_pair[0]
-last_letter = letters_pair[-1]
+first_letter = letters[0]
+last_letter = letters[-1]
 
 # Find index of the first and the last letters in string.ascii_letters
 start_index = string.ascii_letters.index(first_letter)
 end_index = string.ascii_letters.index(last_letter)
 
 # Find missing letters between letters_pair
-new_letters = string.ascii_letters[start_index:end_index + 1]
+new_letters = string.ascii_letters[start_index: end_index + 1]
 
 print(new_letters)
