d593612e165392908203ae6ea9d98ca6844b55ed
Bogdana Matveeva <b.matveeva@epicentrm.com.ua>
2024-08-02 22:24:30 -0700

    Resolving L11 HW2

---
+++ L11/HW_2.py
@@ -0,0 +1,24 @@
+def generate_cube_numbers(end):
+    """
+    Генерує куби чисел, починаючи з 2, поки куб числа <= ніж end
+
+    :end: Верхня межа для кубів чисел
+    :yield: Куб числа
+    """
+    num = 2
+    while True:
+        cube = num ** 3
+        if cube >= end + 1:
+            return
+        yield cube
+        num += 1
+
+
+from inspect import isgenerator
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) == True, "Test0"
+assert list(generate_cube_numbers(10)) == [8], "оскільки воно менше 10."
+assert list(generate_cube_numbers(100)) == [8, 27, 64], "5 у кубі це 125, а воно вже більше 100"
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], "10 у кубі це 1000"
+print("OK")
